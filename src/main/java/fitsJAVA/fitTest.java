6805f46eac6a4e020b3659b0d9f63ef30290902d
abhinav312 <qwertyabhipandey.798@gmail.com>
2023-11-10 03:29:38 +0530

    fitTest modified

--- src/main/java/fitsJAVA/fitTest.java
+++ src/main/java/fitsJAVA/fitTest.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class fitTest {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter name fo the user: ");
-		String name_user=sc.next();
-		System.out.println(name_user);
-		sc.close();
+               //Scanner sc=new Scanner(System.in);
+	       //System.out.println("Enter name fo the user: ");
+		System.out.println("This is my first demo CI/CD");
+		
 			
 	}
 
