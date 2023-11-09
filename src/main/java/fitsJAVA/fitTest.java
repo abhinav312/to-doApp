0cd17dcdfed320b68a7d5bd1d35d8dbffd9e626d
abhinav312 <qwertyabhipandey.798@gmail.com>
2023-11-10 03:24:58 +0530

    first commit

---
+++ src/main/java/fitsJAVA/fitTest.java
@@ -0,0 +1,16 @@
+package fitsJAVA;
+
+import java.util.Scanner;
+
+public class fitTest {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name fo the user: ");
+		String name_user=sc.next();
+		System.out.println(name_user);
+		sc.close();
+			
+	}
+
+}
