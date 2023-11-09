ae90ff7ca64753c7feed09b404fd8a408d562c34
abhinav312 <qwertyabhipandey.798@gmail.com>
2023-11-10 03:38:53 +0530

    QA_branch commit first

--- src/main/java/fitsJAVA/fitTest.java
+++ src/main/java/fitsJAVA/fitTest.java
@@ -7,7 +7,7 @@ public class fitTest {
 	public static void main(String[] args) {
                //Scanner sc=new Scanner(System.in);
 	       //System.out.println("Enter name fo the user: ");
-		System.out.println("This is my first demo CI/CD");
+		System.out.println("This is my first demo CI/CD for QA_branch");
 		
 			
 	}
